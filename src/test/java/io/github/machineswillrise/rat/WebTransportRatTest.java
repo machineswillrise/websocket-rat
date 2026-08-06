d7e980fd523f984f3e29921716d2179b325da52f
MachinesWillRise <machineswillrise@protonmail.com>
2026-08-06 18:22:05 -0400

    Project setup

---
+++ src/test/java/io/github/machineswillrise/rat/WebTransportRatTest.java
@@ -0,0 +1,16 @@
+package io.github.machineswillrise.rat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class WebTransportRatTest
+{
+    @Test
+    @DisplayName("Fake test to change later")
+    void fakeTest()
+    {
+        assertTrue(true);
+    }
+}
