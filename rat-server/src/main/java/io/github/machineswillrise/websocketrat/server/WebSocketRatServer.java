ee04efa13cb192f355668a3c12654979dab1214f
MachinesWillRise <machineswillrise@protonmail.com>
2026-08-06 23:01:42 -0400

    Set up HikariCP

--- rat-server/src/main/java/io/github/machineswillrise/websocketrat/server/WebSocketRatServer.java
+++ rat-server/src/main/java/io/github/machineswillrise/websocketrat/server/WebSocketRatServer.java
@@ -1,9 +1,30 @@
 package io.github.machineswillrise.websocketrat.server;
 
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 public class WebSocketRatServer
 {
+	private DSLContext createDSL(String url)
+	{
+		var config = new HikariConfig();
+		config.setDriverClassName("org.sqlite.JDBC");
+		config.setJdbcUrl(url);
+		config.setMaximumPoolSize(5);
+		config.setConnectionTestQuery("SELECT 1");
+
+		var dataSource = new HikariDataSource(config);
+		return DSL.using(dataSource, SQLDialect.SQLITE);
+	}
+
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
+		var server = new WebSocketRatServer();
+		var dsl = server.createDSL("jdbc:sqlite:" + System.getProperty("user.home") + "/" + "rat.db");
+		dsl.select(DSL.inline("test")).fetch();
 	}
 }
