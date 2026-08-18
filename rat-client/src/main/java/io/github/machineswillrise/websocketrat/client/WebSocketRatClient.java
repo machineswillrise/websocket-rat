f41d6055fbfd0151568f74cd1a569af4a73fe3a5
MachinesWillRise <machineswillrise@protonmail.com>
2026-08-18 09:35:54 -0400

    Remove dumb comment

--- rat-client/src/main/java/io/github/machineswillrise/websocketrat/client/WebSocketRatClient.java
+++ rat-client/src/main/java/io/github/machineswillrise/websocketrat/client/WebSocketRatClient.java
@@ -6,11 +6,6 @@ public class WebSocketRatClient
 {
 	public static void main(String[] args)
 	{
-		/*
-		 * "Because I don't believe that it's really desirable to have
-		 * security on a computer, I shouldn't be willing to help uphold
-		 * the security regime" - Richard Stallman
-		 */
 		if (OperatingSystem.detectCurrentOperatingSystem() == OperatingSystem.MAC_OS)
 		{
 			boolean screenCaptureAllowed = CoreGraphicsLibrary.INSTANCE.CGPreflightScreenCaptureAccess();
